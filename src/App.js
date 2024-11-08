bae9da38f0055c7a40e2660aec4190ddda0fece7
Daniel Preciado <Daniel.Preciado@zilliant.com>
2024-11-07 19:31:09 -0500

    SplitScreen Pattern

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import SplitScreen from "./components/SplitScreen";
+
+const LeftSideComponent = ({ title }) => {
+  return <h2 style={{ backgroundColor: "crimson" }}>{title}</h2>;
+};
+
+const RightSideComponent = ({ title }) => {
+  return <h2 style={{ backgroundColor: "burlywood" }}>{title}</h2>;
+};
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <SplitScreen leftWidth={1} rightWidth={3}>
+      <LeftSideComponent title={"Left"} />
+      <RightSideComponent title={"Right"} />
+    </SplitScreen>
   );
 }
 
